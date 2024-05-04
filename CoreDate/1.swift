c16399f80173ea429b9036892c682fd23fb49086
ChingizT <162159114+ChingizT@users.noreply.github.com>
2024-05-04 18:37:05 +0500

    CoreDate
    CoreDate

---
+++ CoreDate/1.swift
@@ -0,0 +1,14 @@
+//
+//  1.swift
+//  CoreDate
+//
+//  Created by Chingiz on 04.05.2024.
+//
+
+import Foundation
+import CoreData
+
+@objc(TodoListTask)
+public class TodoListTask: NSManagedObject {
+
+}
